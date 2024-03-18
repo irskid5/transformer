83df31d6aa40bcc05ef8beba29c2f89f87de1c0c
Vele Tosevski <vele97@gmail.com>
2024-03-18 19:47:18 -0400

    added decoder module, module lists instead of general python lists, and documentation

--- layers.py
+++ layers.py
@@ -88,14 +88,14 @@ class MultiHeadedAttention(torch.nn.Module):
         self.d_model = d_model
         self.d_k = d_k
         self.d_v = d_v
-        self.query_linear = [torch.nn.Linear(
-            d_model, d_k, bias=False, device=self.device) for _ in range(h)]
-        self.key_linear = [torch.nn.Linear(
-            d_model, d_k, bias=False, device=self.device) for _ in range(h)]
-        self.value_linear = [torch.nn.Linear(
-            d_model, d_v, bias=False, device=self.device) for _ in range(h)]
-        self.attention_layers = [
-            ScaledDotProductAttention(scale) for _ in range(h)]
+        self.query_linear = torch.nn.ModuleList([torch.nn.Linear(
+            d_model, d_k, bias=False, device=self.device) for _ in range(h)])
+        self.key_linear = torch.nn.ModuleList([torch.nn.Linear(
+            d_model, d_k, bias=False, device=self.device) for _ in range(h)])
+        self.value_linear = torch.nn.ModuleList([torch.nn.Linear(
+            d_model, d_v, bias=False, device=self.device) for _ in range(h)])
+        self.attention_layers = torch.nn.ModuleList([
+            ScaledDotProductAttention(scale) for _ in range(h)])
         self.output_linear = torch.nn.Linear(
             h*d_v, d_model, bias=False, device=self.device)
 
@@ -131,3 +131,62 @@ class MultiHeadedAttention(torch.nn.Module):
         output_linear = self.output_linear(concatenated)
 
         return output_linear
+
+
+class TransformerDecoderModule(torch.nn.Module):
+    def __init__(self, d_model: int, d_k: int, d_v: int, d_ff: int, h: int = 1, scale: float | None = None, dropout_p: float = 0.0, activation=torch.relu, device=torch.device("cpu")):
+        """Initializes the transformer decoder module, build according to "Attention is all you need".
+        The transformer decoder architecture stacks multiple of these decoder modules.
+
+        Args:
+            d_model (int): dimension of model
+            d_k (int): dimension of queries and keys
+            d_v (int): dimension of values
+            d_ff (int): intermediate dimension of feedforward sublayer
+            h (int, optional): number of heads. Defaults to 1.
+            scale (float | None, optional): scale to use instead of default. Defaults to None.
+            dropout_p (float, optional): probability for dropout in sublayers. Defaults to 0.0.
+            activation ((input: torch.Tensor) -> torch.Tensor, optional): activation function for feedforward sublayer. Defaults to torch.relu.
+            device (_type_, optional): device to use for computation. Defaults to torch.device("cpu").
+        """
+        super(TransformerDecoderModule, self).__init__()
+        self.device = device
+        self.scale = scale
+        self.h = h
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.activation = activation
+        self.dropout_p = dropout_p
+
+        self.multi_headed_attention = MultiHeadedAttention(
+            d_model, d_k, d_v, h, scale, device)
+
+        self.feed_forward_0 = torch.nn.Linear(d_model, d_ff)
+        self.feed_forward_1 = torch.nn.Linear(d_ff, d_model)
+
+        self.layer_norm_0 = torch.nn.LayerNorm(d_model)
+        self.layer_norm_1 = torch.nn.LayerNorm(d_model)
+
+        self.dropout_0 = torch.nn.Dropout(dropout_p)
+        self.dropout_1 = torch.nn.Dropout(dropout_p)
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor):
+        # MultiHeadedAttention (masked) - Sublayer 0
+        attention = self.multi_headed_attention(x, x, x, mask)
+        attention_dropout = self.dropout_0(attention)
+
+        # Add + LayerNorm
+        residual_0 = attention_dropout + x
+        layer_norm_0 = self.layer_norm_0(residual_0)
+
+        # FeedForward - Sublayer 1
+        ff_0 = self.activation(self.feed_forward_0(layer_norm_0))
+        ff_1 = self.feed_forward_1(ff_0)
+        ff_1_dropout = self.dropout_1(ff_1)
+
+        # Add + LayerNorm
+        residual_1 = ff_1_dropout + layer_norm_0
+        layer_norm_1 = self.layer_norm_1(residual_1)
+
+        return layer_norm_1
